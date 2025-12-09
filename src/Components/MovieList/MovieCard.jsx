d69aacdfa30f6f997e15c52f8015b37a7ba85a3d
Omorowa Edwin <edwinomorowa33@gmail.com>
2025-12-09 14:51:42 +0100

    working on the card file

---
+++ src/Components/MovieList/MovieCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MovieCard.css";
+import dummy from "../../assets/Images/dummy.jpg"; // exact filename & extension
+
+
+const MovieCard = () => {
+  return (
+    <a href="#" className="movie_card">
+      <img src={dummy} alt="Movie Poster" />
+    </a>
+  );
+};
+
+export default MovieCard;
--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import "./MovieList.css";
 import Fire from "../../assets/Images/fire.png";
+import MovieCard from "./MovieCard"; // correct import
 
 const MovieList = () => {
   return (
     <section className="movie_list">
       <header className="movie_list_header">
+        {/* Left side: heading */}
         <h2 className="movie_list_heading">
           Popular <img className="navbar_emoji" src={Fire} alt="fire emoji" />
         </h2>
 
+        {/* Right side: filters + dropdowns */}
         <div className="movie_list_fs">
-            <ul className="movie_filter">
-                <li className="movie_filter_item"> 8+ Star</li>
-                <li className="movie_filter_item"> 7+ Star</li>
-                <li className="movie_filter_item"> 6+ Star</li>
-            </ul>
-           <select name="" id="" className="Movie_sorting">
-            <option value="">SortBy</option>
-            <option value="">Date</option>
-            <option value="">Rating</option>
-           </select>
+          <ul className="movie_filter">
+            <li className="movie_filter_item">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
 
-           <select name="" id="" className="Movie_sorting">
-            <option value="">Ascending</option>
-            <option value="">Decending</option>
-           </select>
+          <select className="Movie_sorting">
+            <option value="">SortBy</option>
+            <option value="date">Date</option>
+            <option value="rating">Rating</option>
+          </select>
+
+          <select className="Movie_sorting">
+            <option value="">Order</option>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
         </div>
       </header>
+
+      <div className="Movie_cards">
+        <MovieCard /> {/* You can repeat this to show multiple cards */}
+      </div>
     </section>
   );
 };
